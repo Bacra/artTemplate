94b22fd23e0e7dc44aacae7f37543a5480fe4886
Bacra Woo <bacra.woo@gmail.com>
2014-12-06 20:16:54 +0800

    Add: compile lib for task

--- tasks/compile.js
+++ tasks/compile.js
@@ -1,3 +1,4 @@
+var compile = require('../build/compile');
 
 module.exports = function(grunt) {
 	grunt.registerMultiTask('artTemplateCompile', 'artTemplate pre compile', function() {
@@ -7,37 +8,17 @@ module.exports = function(grunt) {
 		});
 
 		if (!options.template || !options.template.compile) throw new Error('error template pram, no compile');
-		var template = options.template;
-		var wrapSource = generateWrapSource(options.wrapSource);
 		
 		this.files.forEach(function(file) {
 			var src = file.src[0];
 			var content = grunt.file.read(src);
-			var render = template.compile(content, {filename: src}).toString();
 
-			render = wrapSource(render);
-			grunt.file.write(file.dest+'.js', render);	
+			grunt.file.write(file.dest+'.js', compile(options.template, content, {
+				filename: src,
+				wrapSource: options.wrapSource
+			}));	
 		});	
 	});
 };
 
-
-function generateWrapSource(wrapSource) {
-	if (typeof wrapSource == 'function') return wrapSource;
-
-	switch (wrapSource) {
-		case 'sea':
-		case 'seajs':
-			return function(data) {
-				return 'define(){module.exports = '+data+'}';
-			};
-		case 'node':
-		case 'nodejs':
-			return function(data) {
-				return 'module.exports = '+data;
-			};
-		default:
-			return function(data) {return data};
-	}
-}
 
